6427f53830647fc35463178ea01794706cde4064
Johanna <jandrade731@gmail.com>
2023-03-06 23:18:28 -0500

    Subiendo repositorio

---
+++ parcial/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Card } from "./Card/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
